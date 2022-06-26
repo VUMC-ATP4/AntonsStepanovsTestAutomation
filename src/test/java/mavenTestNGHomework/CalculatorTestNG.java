74a1d4c6102db93a5670f9a62025628d94bc9a4d
antons-stepanovs <104780319+antons-stepanovs@users.noreply.github.com>
2022-06-26 14:25:00 +0300

    Initial commit 1.uzd
    Initial commit 1.uzd

---
+++ src/test/java/mavenTestNGHomework/CalculatorTestNG.java
@@ -0,0 +1,52 @@
+package mavenTestNGHomework;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class CalculatorTestNG {
+
+    Calculator calculator;
+
+    @BeforeSuite
+    public void pirmsTestaInfo() {
+        System.out.println("Tiks testēta klase Calculator");
+    }
+
+
+    @BeforeTest
+    public void pirmsTestaObjekts() {
+        calculator = new Calculator();
+    }
+
+    @BeforeMethod
+    public void pirmsTestaTeksts() {
+        System.out.println("Sākas tests");
+    }
+
+    @AfterClass
+    public void pecTesta() {
+        System.out.println("Visi testi veiksmīgi pabeigti!");
+    }
+
+
+    @Test
+    public void testAdd() {
+         Assert.assertEquals(calculator.add(13, 25), 38);
+    }
+
+    @Test
+    public void testSubstract() {
+        Assert.assertEquals(calculator.substract(10, 5), 5);
+    }
+
+    @Test
+    public void testMultiply() {
+        Assert.assertEquals(calculator.multiply(5, 5), 25);
+    }
+
+    @Test
+    public void testDivision() {
+        Assert.assertEquals(calculator.divide(10, 5), 2);
+    }
+
+}
